3b2e714313860f0ce84ec25e5f3323778c65ecc0
Siddhesh Jondhale <84581657+Siddheshjondhale@users.noreply.github.com>
2025-01-20 21:11:22 +0530

    Increased budget in angular.json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotFoundPageComponent } from './shared/not-found-page/not-found-page.component';
 
 const routes: Routes = [
-  {path:'',
-  loadChildren: () =>
-    import('../app/task-manager/task-manager.module').then((m) => m.TaskManagerModule),
+  { path: '', redirectTo: '/task-manager', pathMatch: 'full' },
+  {
+    path: 'task-manager',
+    loadChildren: () => import('./task-manager/task-manager.module').then(m => m.TaskManagerModule)
   },
-  {path:'404', component: NotFoundPageComponent},
-  {path: '**', redirectTo:'404'}
+  { path: '404', component: NotFoundPageComponent },
+  { path: '**', redirectTo: '404' }
 ];
 
 @NgModule({
